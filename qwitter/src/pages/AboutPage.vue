aa8e24ac784665b21d18207fc43fdd61217dc1bd
dxps <marius.ileana@gmail.com>
2022-06-27 19:38:34 +0300

    qwitter: pages upd

--- qwitter/src/pages/AboutPage.vue
+++ qwitter/src/pages/AboutPage.vue
@@ -1,6 +1,13 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Quasar Twitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita odio
+        repudiandae fugit eveniet eos. Vitae nostrum ea, magnam ipsa vero fugiat
+        nobis consequuntur quos enim, sapiente veniam placeat, facere voluptate?
+      </p>
+    </div>
   </q-page>
 </template>
 
--- qwitter/src/pages/HomePage.vue
+++ qwitter/src/pages/HomePage.vue
@@ -1,9 +1,49 @@
 <template>
   <q-page>
-    <h5>Home</h5>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newTweetContent"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+          class="new-tweet"
+        >
+          <template v-slot:before>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label=" Q Tweet "
+          no-caps
+          no-wrap
+          :disable="!newTweetContent"
+          class="q-mb-lg"
+        />
+      </div>
+    </div>
+    <q-separator size="10px" color="qwitter-dark-sep" />
   </q-page>
 </template>
 
 <script setup lang="ts">
-// ...
+import { ref } from 'vue'
+
+let newTweetContent = ref('')
 </script>
+
+<style lang="scss">
+.new-tweet textarea {
+  font-size: 19px;
+  line-height: 1.4 !important;
+}
+</style>
